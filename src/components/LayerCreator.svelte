c64e78dec70e5796ebdc30b8745918262208313b
Jason Biegel <me@jasonbiegel.com>
2022-03-13 23:20:44 -0400

    updated config interface and applied changes to existing code. Also did some refactoring along the way, including removing the option for OSM as a layer

--- src/components/LayerCreator.svelte
+++ src/components/LayerCreator.svelte
@@ -1,16 +1,14 @@
 <script context="module" lang="ts">
 	import GeoJsonLayer from './GeoJsonLayer.svelte';
     import LayerByIncome from './LayerByIncome.svelte';
-    import Config from '../config';
+    import type { Layer } from '../config';
 
-    export function CreateLayer(config: Config) {
-        const { name, url } = config;
-
-        if (name == "New York City Income") {
-            return {component: LayerByIncome, ...config};
+    export function CreateLayer(layerConfig: Layer) {
+        if (layerConfig.name === 'New York City Income') {
+            return {component: LayerByIncome, ...layerConfig};
         }
 
-        return {component: GeoJsonLayer, ...config};
+        return {component: GeoJsonLayer, ...layerConfig};
     }
 
 </script>
--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,27 @@
-export interface Layer {
-  url: string,
-  name: string
-}
-
 export default interface Config {
   mapboxAccessToken: string,
   latLng: [number, number],
   zoom: number,
-  layers: Layer[]
+  layers: Layer[],
+  stories: Story[]
+}
+
+export interface Layer {
+  id: string,
+  name: string,
+  url: string,
+  type: 'geojson'|'raster',
+  controlProperties?: ControlProperty[]
+}
+
+export interface Story {
+  title: string,
+  content: string
+}
+
+export interface ControlProperty {
+  id: string,
+  type: 'slider',
+  key?: string,
+  prefixKey?: string
 }
