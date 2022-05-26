30597ef3481d3c641cd313da6b5fa07d3b0651bf
Jason Biegel <me@jasonbiegel.com>
2022-05-26 18:44:03 -0400

    story images with pop-up support

--- src/components/Story.svelte
+++ src/components/Story.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import {LatLng, Map} from 'leaflet';
-    import { onDestroy } from 'svelte';
+    import { onDestroy, onMount } from 'svelte';
     import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
 
     import { mapStore, mapStateStore } from '../stores';
     import config, { MapState } from '../config';
-
+    import initImagePopup from '../popup';
 
     let map: Map|undefined = undefined;
 
@@ -17,13 +17,29 @@
 
     onDestroy(unsubscribe);
 
+    onMount(async () => {
+        initImageZoom();
+    });
+
     function setMapState(mapState: MapState): void {
         mapStateStore.set(mapState);
         map?.flyTo(new LatLng(mapState.lat, mapState.lng), mapState.zoom);
     }
+
+    function initImageZoom() {
+        const storyImages = document.querySelectorAll('#story-container img');
+        if (storyImages) {
+            for (const storyImage of Array.from(storyImages)) {
+                storyImage.addEventListener('click', (event: Event) => {
+                    initImagePopup(event);
+                });
+            }
+        }
+    }
+
 </script>
 
-<div class="accordion-container">
+<div id="story-container" class="accordion-container">
     <Accordion>
         {#each config.stories as story}
             <Panel on:click={() => setMapState(story.mapState)}>
