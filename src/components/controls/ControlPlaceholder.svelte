5faa5a4d626570661f75c6457e9baed902e61b3f
Tom Z <tzinckgraf@gmail.com>
2022-07-11 22:54:34 -0400

    main - added the control placeholder when there are no controls

---
+++ src/components/controls/ControlPlaceholder.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Control from './Control.svelte';
+  import type { ControlProperty, LayerProperty } from '../../config';
+
+  const controlProperty = {
+    id: '_placeholder',
+    name: 'Add Layers For Custom Controls',
+    subtitle: ''
+  }
+
+  const layerProperty = {
+    name: 'Add Layers For Custom Controls',
+    url: '',
+    type: ''
+  }
+</script>
+
+<Control controlProperty={controlProperty} layerProperty={layerProperty} >
+    <span slot="values"> </span>
+    <span slot="control">
+        <div>
+            <p>
+                Custom controls will be available when selecting custom layers with controls.
+                Not all layers will have custom controls.
+            </p>
+
+            <p>
+            Go to Add Layers and choose some layers to add to the map.
+            </p>
+
+            <p>
+            Layers will also be added with stories. Check the stories tab out for more details.
+            </p>
+        </div>
+    </span>
+</Control>
